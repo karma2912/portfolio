9fd9b401f60b3d9b84ef292c5816cacb43c7ae53
karma2912 <rajakyash23@gmail.com>
2024-09-22 15:57:01 +0530

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
           <label htmlFor="message" className="leading-7 text-sm text-white">Message</label>
           <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
         </div>
-        <button className="w-full text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+        <button className="w-full text-white bg-purple-400 border-0 py-2 px-6 focus:outline-none hover:bg-purple-300 rounded text-lg">Submit</button>
         <p className="text-xs text-white mt-3 text-center" >There is a way for everything</p></div>}
       </form>
     </div>
