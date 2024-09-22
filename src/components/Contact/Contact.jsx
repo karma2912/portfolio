09f886c8e7dc5bf86e7c13e6d5af61516a3c4be6
karma2912 <rajakyash23@gmail.com>
2024-09-22 16:20:55 +0530

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
       <iframe width="100%" height="100%" frameBorder="0" marginHeight="0" marginWidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{filter:' black(1) contrast(1.2) opacity(0.4)'}}></iframe>
     </div>
     <div className="container px-5 py-24 mx-auto flex">
-      <form className="lg:w-1/3 md:w-1/2 bg-black text-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md" onSubmit={onSubmit}>
+      <form className="lg:w-1/3 md:w-1/2 h-[30rem] bg-black text-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md" onSubmit={onSubmit}>
         {!toggle && <div className=""> <h2 className="text-white text-lg mb-1 font-medium title-font">Mail Me</h2>
         <p className="leading-relaxed mb-5 text-white">Your Emails will be read by Our team</p>
         <div className="relative mb-4">
@@ -47,6 +47,9 @@ const Contact = () => {
         </div>
         <button className="w-full text-white bg-purple-400 border-0 py-2 px-6 focus:outline-none hover:bg-purple-300 rounded text-lg">Submit</button>
         <p className="text-xs text-white mt-3 text-center" >There is a way for everything</p></div>}
+        {toggle && <div className='bg-green-400 h-[30rem] w-full flex justify-center items-center'>
+          <div className='text-3xl font-semibold'>Sent!!!</div>
+          </div>}
       </form>
     </div>
   </section>
