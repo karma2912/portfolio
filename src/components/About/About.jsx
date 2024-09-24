a1047815d30a13f6d4b6d59295d3f5d2e195f3fa
karma2912 <rajakyash23@gmail.com>
2024-09-24 12:28:03 +0530

    comit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,10 +16,9 @@ const About = () => {
           </div>
         </div>
         <div className="h-full w-full md:pt-[30rem] pt-24  flex flex-col md:gap-28 gap-14 justify-center items-center">
-         
-          <div className="w-11/12 text-md text-center flex md:flex-row flex-col justify-around items-center">
-           <div className="h-[29rem] md:h-[40rem] md:w-1/3 w-full flex flex-col justify-evenly items-center border-2 border-slate-400 p-2 rounded-xl" ><div className="text-5xl font-bold meet ">Meet Yash Rajak,</div>
-           <div className="text-lg meet ">Heyy , I am Yash a Web Designer Passionate for my work .I help businesses to go online thorugh the modern Websites, Whether its an Ecommerce , Info or any  type of website .I have gain all the skills and mastered through creating various types of projects for my personal practice as well as for my clients and All of these i have learned,practiced and done at the age of 17.</div></div>
+         <div className="w-11/12 text-md flex md:flex-row flex-col justify-around items-center">
+           <div className="h-[29rem] md:h-[40rem] md:w-1/3 w-full flex flex-col justify-evenly text-center  border-2 border-slate-400 p-2 rounded-xl" ><div className="text-5xl font-bold meet ">Meet Yash Rajak,</div>
+           <div className="text-lg text-start meet ">Heyy , I am Yash a Web Designer Passionate for my work .I help businesses to go online thorugh the modern Websites, Whether its an Ecommerce , Info or any  type of website .I have gain all the skills and mastered through creating various types of projects for my personal practice as well as for my clients and All of these i have learned,practiced and done at the age of 17.</div></div>
            <div className="h-[29rem] md:h-[40rem] md:w-1/3 w-full flex justify-center items-center"><img className="md:w-full w-[28rem]" src={ay}></img></div>
         </div>
       </div>
