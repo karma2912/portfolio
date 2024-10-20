a3e1dd41b5215b5b6d55b0860849816539b6ffe3
karma2912 <rajakyash23@gmail.com>
2024-10-20 14:14:37 +0530

    commit

--- src/components/NewNavbar.jsx
+++ src/components/NewNavbar.jsx
@@ -4,11 +4,11 @@ const NewNavbar = () => {
   return (
     <div className='w-full h-24 bg-transparent pt-4 flex md:justify-evenly justify-around items-center z-40 top-0 sticky'>
         <div className='w-full h-full flex justify-between items-end'>
-         <div className='text-5xl pl-16'>
+         <div className='text-4xl pl-16'>
             Y.
          </div>
          <div className='pr-20'>
-         <i className="fa-solid fa-bars text-3xl "></i>
+         <i className="fa-solid fa-bars text-2xl "></i>
          </div>
         </div>
     </div>
